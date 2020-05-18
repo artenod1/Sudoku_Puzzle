47374d023f07349abf0e3657847b1ee2d5ec9c93
Dominic Arteno <57507925+artenod1@users.noreply.github.com>
2020-05-18 12:18:17 -0400

    Add files via upload

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -5,20 +5,30 @@ from check_position import check_position
 
 
 def solve(puzzle):
-    find_zero = get_zero_index(puzzle)
-    if not find_zero:
+	"""
+	Main function that solves input puzzle.
+	"""
+    
+	# Use a backtracking recursion algorithm
+
+	# Base case (no zeros in puzzle)
+	find_zero = get_zero_index(puzzle)
+	if not find_zero:
         # print(puzzle)
-        return True
+		return True
 
-    else:
-        row = find_zero[0]
-        column = find_zero[1]
-        for num in range(1, 10):
-            if check_position(puzzle, row, column, num):
-                puzzle[row][column] = num
-                print(puzzle)
+    # Recursion
+	else:
+	    row = find_zero[0]
+	    column = find_zero[1]
+	    for num in range(1, 10):
+	        if check_position(puzzle, row, column, num):
+	            puzzle[row][column] = num
+	            # print puzzle so each step in process is printed
+	            print(puzzle)
 
-                if solve(puzzle):
-                    return True
+	            if solve(puzzle):
+	                return True
 
-                puzzle[row][column] = 0
+	            # Number did not work, reset to zero.
+	            puzzle[row][column] = 0
